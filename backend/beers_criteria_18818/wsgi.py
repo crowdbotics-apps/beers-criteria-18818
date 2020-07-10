9b166ba09fcec19a3bed7091790c3930783bca11
Crowdbotics <team@crowdbotics.com>
2020-07-10 19:40:14 +0000

    Update models

--- backend/beers_criteria_18818/wsgi.py
+++ backend/beers_criteria_18818/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beers_criteria_18818.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "beers_criteria_18818.settings")
 
 application = get_wsgi_application()
